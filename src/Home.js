f69355f57c2c83b2de46df6bf7c326cfd130a552
Maung Ko Ko <mwkoko2020@gmail.com>
2023-07-02 18:38:36 -0600

    fetching Data

--- src/Home.js
+++ src/Home.js
@@ -2,26 +2,7 @@ import { useEffect, useState } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      title: 'My new Website',
-      body: 'lorem ipsum...',
-      author: 'Maung',
-      id: 1,
-    },
-    {
-      title: 'Welcome party',
-      body: 'lorem ipsum...',
-      author: 'Ko Ko',
-      id: 2,
-    },
-    {
-      title: 'Web dev top tips',
-      body: 'lorem ipsum...',
-      author: 'Maung',
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
   const [name, setName] = useState('Ko Ko');
 
@@ -31,15 +12,24 @@ const Home = () => {
   };
 
   useEffect(() => {
-    console.log('use effect');
-    console.log(name);
-  }, [name]);
+    fetch('http://localhost:8000/blogs')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      });
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-      <button onClick={() => setName('Maung')}>change name</button>
-      <p>{name}</p>
+      {blogs && (
+        <BlogList
+          blogs={blogs}
+          title="All Blogs!"
+          handleDelete={handleDelete}
+        />
+      )}
     </div>
   );
 };
