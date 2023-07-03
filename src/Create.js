84d1183343f9d87049fb65fb2764cd373315dcb7
Maung Ko Ko <mwkoko2020@gmail.com>
2023-07-03 01:24:00 -0600

    add form

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,41 @@
+import { useState } from 'react';
+
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('Maung');
+  const handleSubmit = (event) => {};
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(event) => setBody(event.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select
+          value={author}
+          onChange={(event) => setAuthor(event.target.value)}
+        >
+          <option value="Maung">Maung</option>
+          <option value="Ko Ko">Ko Ko</option>
+        </select>
+        <button>Add Blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
+      </form>
     </div>
   );
 };
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,28 +1,35 @@
 import { useState, useEffect } from 'react';
+
 const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    setTimeout(() => {
-      fetch(url)
-        .then((res) => {
-          if (!res.ok) {
-            throw Error('Could not fetch the data for that resource');
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setData(data);
-          setIsPending(false);
-          setError(null);
-        })
-        .catch((err) => {
+    const abortCont = new AbortController();
+
+    fetch(url, { signal: abortCont.signal })
+      .then((res) => {
+        if (!res.ok) {
+          throw Error('Could not fetch the data for that resource');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        if (err.name === 'AbortError') {
+          console.log('fetch aborted');
+        } else {
           setIsPending(false);
           setError(err.message);
-        });
-    });
+        }
+      });
+
+    return () => abortCont.abort();
   }, [url]);
 
   return { data, isPending, error };
