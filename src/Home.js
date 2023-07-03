96511b2c7eb27d802a4837aaaf3efca689bd4388
Maung Ko Ko <mwkoko2020@gmail.com>
2023-07-02 18:47:10 -0600

    Add loading message

--- src/Home.js
+++ src/Home.js
@@ -3,13 +3,7 @@ import BlogList from './BlogList';
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
-
-  const [name, setName] = useState('Ko Ko');
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
@@ -18,18 +12,14 @@ const Home = () => {
       })
       .then((data) => {
         setBlogs(data);
+        setIsPending(false);
       });
   }, []);
 
   return (
     <div className="home">
-      {blogs && (
-        <BlogList
-          blogs={blogs}
-          title="All Blogs!"
-          handleDelete={handleDelete}
-        />
-      )}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
 };
